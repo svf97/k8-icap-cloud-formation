3ff2c2fe66a2aee6d41d3375456aacbb138a2c20
Gurunath <guru9794@gmail.com>
2021-02-12 16:10:04 +0530

    Removed profile argument

--- icap-controller/us-east-2/delete-stack.sh
+++ icap-controller/us-east-2/delete-stack.sh
@@ -1,9 +1,8 @@
 STACK_NAME=$1
 REGION="us-east-2"
-PROFILE="${2:-default}"
+#PROFILE="${2:-default}"
 #PROFILE="785217600689_AdministratorAccess"
 
 aws cloudformation delete-stack                                         \
                 --stack-name ${STACK_NAME}                              \
                 --region $REGION                                        \
-                --profile $PROFILE
